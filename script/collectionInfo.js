66723108cb6a690a5b034ae538423d915dd8d75e
Yeonsu <banananjuseyo@gmail.com>
2020-12-03 21:06:53 +0900

    collections

---
+++ script/collectionInfo.js
@@ -0,0 +1,68 @@
+export const collectionInfo = [
+  {
+    name: "collection1",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/1.png",
+  },
+  {
+    name: "collection2",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/2.jpg",
+  },
+  {
+    name: "collection5",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/5.png",
+  },
+  {
+    name: "collection4",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/4.png",
+  },
+  {
+    name: "collection3",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/3.jpg",
+  },
+  {
+    name: "collection6",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/6.png",
+  },
+  {
+    name: "collection7",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/7.png",
+  },
+  {
+    name: "collection8",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/8.png",
+  },
+  {
+    name: "collection9",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/9.png",
+  },
+  {
+    name: "collection10",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/10.png",
+  },
+  {
+    name: "collection11",
+    desc: "infoinfo",
+    time: 2020,
+    src: "../img/collection/11.png",
+  },
+];
---
+++ script/collections.js
@@ -0,0 +1,60 @@
+import { collectionInfo } from "./collectionInfo.js";
+
+const $collectionList = document.querySelector("#collectionList"),
+  $modal = document.querySelector("#modal"),
+  $modalCloseBtn = $modal.querySelector("button");
+
+const setCollectionList = () => {
+  collectionInfo.map((item) => {
+    var $collection = document.createElement("div"),
+      $textBox = document.createElement("div");
+    $collection.classList.add("collection");
+    $textBox.classList.add("textBox");
+
+    var $h3 = document.createElement("h3"),
+      $span = document.createElement("span"),
+      $p = document.createElement("p"),
+      $img = document.createElement("img");
+
+    var randomNum = Math.floor(Math.random() * 100);
+
+    $h3.innerText = item.name;
+    $span.innerText = item.time;
+    $p.innerText = item.desc;
+    $img.src = item.src;
+
+    $textBox.appendChild($h3);
+    $textBox.appendChild($span);
+    $textBox.appendChild($p);
+
+    $collection.appendChild($img);
+    $collection.appendChild($textBox);
+
+    $collectionList.appendChild($collection);
+
+    $collection.style.transform = `translateY(${randomNum}px)`;
+
+    $img.addEventListener("click", setModal);
+  });
+};
+
+const setModal = async (e) => {
+  await setModalText(e);
+  toggleModal();
+};
+const toggleModal = () => {
+  $modal.classList.toggle("active");
+  document.body.classList.toggle("modalFix");
+};
+
+const setModalText = (e) => {
+  $modal.querySelector("img").src = e.target.currentSrc;
+  $modal.querySelector(".textBox").innerHTML = e.target.nextSibling.innerHTML;
+};
+
+$modalCloseBtn.addEventListener("click", toggleModal);
+
+init();
+function init() {
+  setCollectionList();
+}
